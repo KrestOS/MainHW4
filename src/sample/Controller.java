6b26e4d6193324d5d5bab4433f26880d875fffdd
[KrestOS] <[krestik9@yandex.ru]>
2020-09-03 15:09:35 +0300

    hw4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
 public class Controller {
+
+    @FXML
+    public TextArea textArea;
+    @FXML
+    public TextField textField;
+
+    public void onTextField(){
+       doSend();
+    }
+
+    public void onClickBtn(){
+        doSend();
+    }
+
+    public void doSend(){
+        if (textField.getText().length() != 0){
+
+            textArea.appendText(textField.getText() + "\n");
+
+        }
+        textField.requestFocus();
+        textField.clear();
+    }
 }
